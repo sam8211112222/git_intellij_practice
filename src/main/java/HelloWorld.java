a49c0c86d273ccace74d36dcb91dea164ae367e0
morrismao <shady28612458@gmail.com>
2022-10-04 10:16:27 +0800

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("HELLO WORLD");
+        System.out.println("Hi");
+        System.out.println("Hi2");
+        System.out.println("Hi3");
     }
 }
